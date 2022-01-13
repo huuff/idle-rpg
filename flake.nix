c213bd55be3c4c00f1f542e0e2c0fe4ae7a3e877
Haf <haf@protonmail.ch>
2022-01-13 14:03:41 +0100

    added a flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in with pkgs;
+  {
+    devShell.${system} = mkShell {
+      buildInputs = [ nodejs-17_x yarn ];
+    };
+  };
+}
