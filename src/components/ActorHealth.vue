d4f523cfa12aa7c3555525c14629638bb0af774e
Haf <haf@protonmail.ch>
2022-01-13 16:16:16 +0100

    decent progress bar animation

--- src/components/ActorHealth.vue
+++ src/components/ActorHealth.vue
@@ -1,13 +1,24 @@
 <template>
-  <progress class="progress is-primary" :value="actor.currentHealth" :max="actor.stats.maxHealth">
+  <progress class="progress is-primary" :value="shownValue" :max="actor.stats.maxHealth">
     {{ actor.currentHealth }}
   </progress>
 </template>
 
 <script setup lang="ts">
+import { watch, ref } from "vue";
 import { Actor } from "@/battle/actor";
+import animejs from "animejs";
 
 const props = defineProps<{
   actor: Actor;
 }>();
+
+const shownValue = ref(props.actor.currentHealth);
+
+watch(() => props.actor.currentHealth, (newValue) => animejs(({
+  targets: shownValue,
+  value: newValue,
+  easing: "linear",
+  autoplay: true,
+})));
 </script>
