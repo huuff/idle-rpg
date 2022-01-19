f7447429dcfc5d3b54f01c75a8672da10c09c608
Haf <haf@protonmail.ch>
2022-01-19 16:02:35 +0100

    more robust settlement keys

--- src/components/scenes/SettlementView.vue
+++ src/components/scenes/SettlementView.vue
@@ -2,15 +2,13 @@
 <p class="title has-text-dark">Resting in {{ location.name }}</p>
 
 <button v-for="destination of possibleDestinations"
-        :key="destination.to.name"
+        :key="`${destination.through.name}-${destination.to.name}`"
         @click="goTo(destination)"
         class="button is-primary"
 >Go to {{ destination.to.name }}</button>
 </template>
 
 <script setup lang="ts">
-// TODO: Maybe button's keys should include zone if there are
-// more than one path to somewhere
 import { computed } from "vue";
 import { MapLocation, TravelOption } from "@/map/game-map";
 import { storeToRefs } from "pinia";
