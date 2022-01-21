67e88b8a694ef24063d17953b2987518f19e22e2
Haf <haf@protonmail.ch>
2022-01-21 21:22:08 +0100

    made the game take the appropriate height

--- TODO.md
+++ TODO.md
@@ -2,5 +2,6 @@
 * A view to show player stats
 * Actual game over
 * Initial screen to choose a character
-* Manage to get the tiles to take the full vertical space
+* Get the bar of the travel decision duration to be at the bottom
+* Rest is still active when departing manually while resting!
 * Disable asking for a travel decision on autoplay
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
-<div class="hero is-fullheight is-info is-bold">
-  <div class="hero-body">
-    <div class="container">
-      <location-indicator />
-      <div class="tile is-ancestor">
-        <div class="tile is-4 is-vertical is-parent">
-          <div class="tile is-child box">
-            <p class="title has-text-dark">Player</p>
-            <p class="subtitle has-text-dark">Level {{ player.level }}</p>
-            <span>Health</span>
-            <health-bar :creature="player" />
-            <span>Experience</span>
-            <animated-bar :current="player.currentExp" :max="player.requiredExp" class="is-info"/>
-          </div>
-          <div v-if="secondaryView" class="tile is-child box">
-            <secondary-view />
-          </div>
+  <div class="container game-container">
+    <location-indicator />
+    <div class="tile is-ancestor tiles-contents">
+      <div class="tile is-4 is-vertical is-parent">
+        <div class="tile is-child box">
+          <p class="title has-text-dark">Player</p>
+          <p class="subtitle has-text-dark">Level {{ player.level }}</p>
+          <span>Health</span>
+          <health-bar :creature="player" />
+          <span>Experience</span>
+          <animated-bar :current="player.currentExp" :max="player.requiredExp" class="is-info"/>
         </div>
-        <div class="tile is-parent">
-          <div class="tile is-child box">
-            <main-view /> 
-          </div>
+        <div v-if="secondaryView" class="tile is-child box">
+          <secondary-view />
+        </div>
+      </div>
+      <div class="tile is-parent">
+        <div class="tile is-child box">
+          <main-view /> 
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script setup lang="ts">
@@ -40,3 +36,19 @@ const { player, } = storeToRefs(useMainStore());
 const { mainView, secondaryView } = storeToRefs(useSceneStore());
 
 </script>
+
+<style>
+body, body, #app {
+  height: 100vh;
+  background-color: hsl(204, 86%, 53%);
+}
+
+.game-container {
+  height: 90vh;
+  padding: 10vh 0;
+}
+
+.tiles-contents {
+  height: 100%;
+}
+</style>
