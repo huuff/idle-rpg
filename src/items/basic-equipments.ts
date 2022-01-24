0a9766da8d26e7ccdff255a6769d71e8005e9ea6
Haf <haf@protonmail.ch>
2022-01-24 21:18:31 +0100

    fixed it somehow

--- src/items/basic-equipments.ts
+++ src/items/basic-equipments.ts
@@ -15,26 +15,32 @@ type JsonType = {[itemName in EquipmentName]: JsonValue};
 export type BasicEquipments = {[name in EquipmentName]: EquipmentItem};
 
 
-// STUCK: WHy the fuck won't it narrowwwwww
+function valueToEquipmentItem(val: JsonValue): EquipmentItem {
+  const type = val.type;
+  if (type !== "equipment") {
+    throw new Error(`item type is not 'equipment': ${JSON.stringify(val)}`)
+  } 
+
+  const slot = val.slot;
+  if (!isEquipmentSlot(slot)) {
+    throw new Error(`${val.slot} is not a valid equipment slot type!`)
+  }
+
+  const statsInput = val.stats;
+  if (!isStatsInput(statsInput)) {
+    throw new Error(`Not valid stats: ${JSON.stringify(val.stats)}`);
+  }
+
+  return {
+    ...val,
+    type,
+    slot,
+    stats: new StatsImpl(val.stats),
+  }
+}
+
 function loadJson(json: JsonType): BasicEquipments {
-  return mapValues(json, (val: JsonValue) => {
-    if (val.type !== "equipment") {
-      throw new Error(`item type is not 'equipment': ${JSON.stringify(val)}`)
-    } 
-
-    if (!isEquipmentSlot(val.slot)) {
-      throw new Error(`${val.slot} is not a valid equipment slot type!`)
-    }
-
-    if (!isStatsInput(val.stats)) {
-      throw new Error(`Not valid stats: ${JSON.stringify(val.stats)}`);
-    }
-    
-    return {
-      ...val,
-      stats: new StatsImpl(val.stats)
-    } as EquipmentItem;
-  });
+  return mapValues(json, valueToEquipmentItem);
 }
 
 export const basicEquipments: BasicEquipments = loadJson(basicEquipmentsJson);
